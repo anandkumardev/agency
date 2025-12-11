6a03fa7e0c9fb5f0432d6e83932c299b87272434
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-11 16:03:08 +0530

    add theme toggle btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
   return (
-   <div className="dark:bg-black relative">
-    <Navbar theme={theme} setTheme={setTheme}/>
-   </div>
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
   );
 }
 export default App;
