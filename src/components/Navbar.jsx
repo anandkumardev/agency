d58861c9b8d561788b8f294646dfcb2f34ff1d9b
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-11 19:39:22 +0530

    update background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ theme, setTheme }) => {
   const [sidebarOpen, setSidebarOpen] = useState("");
 
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-2 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-neutral-950/70">
+    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-2 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-black/70">
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         className="w-28 sm:w-34"
