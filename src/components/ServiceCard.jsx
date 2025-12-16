276f7628441231665c7a6f609a0f1d9d3248b024
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-16 17:38:46 +0530

    updated

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -13,7 +13,7 @@ function ServiceCard({ service, index }) {
 
   return (
     <div
-      className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
+      className="relative overflow-hidden max-w-lg m-3 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
       ref={divRef}
